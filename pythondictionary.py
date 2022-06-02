8d80438132b48b9bf368337f11de6967dbab5e3f
Mundurujoana <mundujoana@gmail.com>
2022-06-02 08:23:39 +0300

    first commit

---
+++ pythondictionary.py
@@ -0,0 +1,12 @@
+#Numbers from 10 to 15
+start = 10
+stop = 50
+step = 5
+for i in range(1, 11, 3):
+    print(i)
+list1 = ['Jessa', 'Emma']
+listb =[]
+for i in list1:
+    if (len(i))<=3:
+      i.append(listb)
+      print(listb)
